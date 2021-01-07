06707e3660688551c56ecc85f87c14a84f70affd
MarleenEliza <62828450+MarleenEliza@users.noreply.github.com>
2021-01-07 23:31:22 +0900

    added basic functionalities for score keeping and reset

---
+++ Score_Practice/script.js
@@ -0,0 +1,55 @@
+// プレイヤー
+const p1 = {
+    score: 0,
+    btn: document.getElementById("p1-btn"),
+    display: document.getElementById("p1-display")
+};
+
+const p2 = {
+    score: 0,
+    btn: document.getElementById("p2-btn"),
+    display: document.getElementById("p2-display")
+};
+
+const resetBtn = document.getElementById("reset-btn");
+resetBtn.addEventListener("click", function () {reset()});
+
+p1.btn.addEventListener("click", function () { updateScore(p1,p2) });
+p2.btn.addEventListener("click", function () { updateScore(p2,p1) });
+
+
+let isGameOver = false;
+let winningScore = 3;
+
+function updateScore(player, opponent) {
+     if(!isGameOver) {
+        addScore(player);
+        if (player.score === winningScore) {
+            endGame(player, opponent);
+        }
+    }
+}
+
+function addScore(player) {
+    player.score += 1;
+    player.display.textContent = player.score;
+}
+
+function endGame(player, opponent) {
+    isGameOver = true;
+    player.display.classList.add('has-text-success');
+    opponent.display.classList.add('has-text-danger');
+    player.btn.disabled = true;
+    opponent.btn.disabled = true;
+}
+
+function reset() {
+    isGameOver = false;
+
+    for(p of [p1,p2]){
+        p.score = 0;
+        p.btn.disabled = false;
+        p.display.textContent = 0;
+        p.display.classList.remove('has-text-success','has-text-danger');
+    };
+}
